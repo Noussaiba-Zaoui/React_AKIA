87bf2317aa41c7c074de9673442de939b7ea44b6
ImeneLEG <imene.legsir@gmail.com>
2024-01-03 16:29:37 +0100

    black friday

--- react-ikea/src/components/home/Sale/Sale.js
+++ react-ikea/src/components/home/Sale/Sale.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Image from "../../designLayouts/Image";
 
-import pic2 from "../../../assets/images/products/pic2.webp";
-import picguitar from "../../../assets/images/products/picguitar.png";
-import etagere from "../../../assets/images/products/etagere.webp";
+import pics2 from "../../../assets/images/products/pics2.png";
+import picsguitar from "../../../assets/images/products/picsguitar.png";
+import etageres from "../../../assets/images/products/etageres.png";
 
 const Sale = () => {
   const imageStyle = {
@@ -14,21 +14,26 @@ const Sale = () => {
   };
 
   return (
-    <div className="py-20 flex flex-col md:flex-row items-center justify-between gap-4 lg:gap-10">
-      <div className="w-full md:w-1/3 h-full">
-        <Link to="/shop">
-          <Image style={imageStyle} imgSrc={pic2} />
-        </Link>
-      </div>
-      <div className="w-full md:w-1/3 h-cover">
-        <Link to="/shop">
-          <Image style={imageStyle} imgSrc={picguitar} />
-        </Link>
-      </div>
-      <div className="w-full md:w-1/3 h-full">
-        <Link to="/shop">
-          <Image style={imageStyle} imgSrc={etagere} />
-        </Link>
+    <div className="py-20 flex flex-col items-left">
+      <h2 className="text-3xl font-bold mb-8 text-primeColor">
+        Black Friday Reduction
+      </h2>
+      <div className="flex flex-col md:flex-row items-center justify-between gap-4 lg:gap-10">
+        <div className="w-full md:w-1/3 h-full">
+          <Link to="/shop">
+            <Image style={imageStyle} imgSrc={pics2} />
+          </Link>
+        </div>
+        <div className="w-full md:w-1/3 h-cover">
+          <Link to="/shop">
+            <Image style={imageStyle} imgSrc={picsguitar} />
+          </Link>
+        </div>
+        <div className="w-full md:w-1/3 h-full">
+          <Link to="/shop">
+            <Image style={imageStyle} imgSrc={etageres} />
+          </Link>
+        </div>
       </div>
     </div>
   );
