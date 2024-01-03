0d05fd9675783e6da0d9089daa0df30f628caf91
noussaiba <noussaibazaoui19@gmail.com>
2024-01-03 03:24:48 +0000

    monatge fixed

--- react-ikea/src/components/home/YearProduct/YearProduct.js
+++ react-ikea/src/components/home/YearProduct/YearProduct.js
@@ -1,5 +1,3 @@
-// YearProduct.js
-
 import React from "react";
 import { Link } from "react-router-dom";
 import montagepic from "../../../assets/images/products/montagepic.png";
@@ -10,12 +8,12 @@ import Montage from "./Montage"; // Import Montage component
 const YearProduct = () => {
   return (
     <Link to="/montage">
-      <div className="w-full h-80 mb-40 bg-[#f3f3f3] md:bg-transparent relative font-titleFont flex flex-col md:flex-row">
+      <div className="w-full h-80 mb-8 md:mb-40 bg-[#f3f3f3] md:bg-transparent relative font-titleFont flex flex-col md:flex-row">
         <Image
-          className="max-w-full h-auto object-contain md:w-1/3 md:h-cover md:object-right "
+          className="max-w-full h-auto object-contain md:w-1/3 md:h-cover md:object-right"
           imgSrc={montagepic}
         />
-        <div className="w-full md:w-2/3 xl:w-1/2 h-80 absolute px-4 md:px-0 top-0 right-0 flex flex-col items-start gap-10 justify-center">
+        <div className="w-full md:w-2/3 xl:w-1/2 h-80 absolute px-4 md:px-8 lg:px-12 top-0 right-0 flex flex-col items-start gap-6 justify-center">
           <h1 className="text-3xl font-semibold text-primeColor">
             Assembly
           </h1>
